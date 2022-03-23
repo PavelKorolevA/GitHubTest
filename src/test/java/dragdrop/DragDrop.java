9914b29cd97dffb375cab2a58723e7375ceac8a9
p.korolev <amlerok96@gmail.com>
2022-03-23 21:55:45 +0300

    added DragDrop

---
+++ src/test/java/dragdrop/DragDrop.java
@@ -0,0 +1,19 @@
+package dragdrop;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragDrop {
+
+    @Test
+    void DragDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-b").shouldHave(text("A"));
+        $("#column-a").shouldHave(text("B"));
+            }
+}
